3a48d5b77611ab412673218871f2288e8cb988fb
screfy <screfy@pm.me>
2022-03-04 15:27:16 +0100

    feat: add `email` validator

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from './validators/str';
 export * from './validators/num';
 export * from './validators/bool';
 export * from './validators/port';
+export * from './validators/email';
 
 export { createValidator };
 export default parse;
---
+++ src/validators/email.ts
@@ -0,0 +1,13 @@
+import { createValidator } from '../tsenv';
+import { InvalidVariableError } from '../errors';
+
+const EMAIL_EXPRESSION =
+  /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+
+export const email = createValidator<string>((name, input) => {
+  if (!EMAIL_EXPRESSION.test(input)) {
+    throw new InvalidVariableError(name, `invalid email address`);
+  }
+
+  return input;
+});

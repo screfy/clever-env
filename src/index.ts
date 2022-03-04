afe3f90969dddf3624d04a476ee1ae332d22167e
screfy <screfy@pm.me>
2022-03-04 15:14:55 +0100

    feat: add `bool` validator

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { parse, createValidator } from './tsenv';
 export * from './types';
 export * from './validators/str';
 export * from './validators/num';
+export * from './validators/bool';
 
 export { createValidator };
 export default parse;
---
+++ src/validators/bool.ts
@@ -0,0 +1,15 @@
+import { InvalidVariableError } from '../errors';
+import { createValidator } from '../tsenv';
+
+export const bool = createValidator<boolean>((name, input) => {
+  switch (input) {
+    case 'true':
+    case '1':
+      return true;
+    case 'false':
+    case '0':
+      return false;
+    default:
+      throw new InvalidVariableError(name, `invalid boolean value`);
+  }
+});

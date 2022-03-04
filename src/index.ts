7c3ed773f980368e6946e1d5cfe6d7f4e61e1eca
screfy <screfy@pm.me>
2022-03-04 15:20:58 +0100

    feat: add `port` validator

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from './types';
 export * from './validators/str';
 export * from './validators/num';
 export * from './validators/bool';
+export * from './validators/port';
 
 export { createValidator };
 export default parse;
---
+++ src/validators/port.ts
@@ -0,0 +1,13 @@
+import { InvalidVariableError } from '../errors';
+import { createValidator } from '../tsenv';
+import { parseNumber } from '../utils';
+
+export const port = createValidator<number>((name, input) => {
+  const value = parseNumber(name, input);
+
+  if (value % 1 !== 0 || value < 1 || value > 65535) {
+    throw new InvalidVariableError(name, 'out of TCP range [1, 65535]');
+  }
+
+  return value;
+});

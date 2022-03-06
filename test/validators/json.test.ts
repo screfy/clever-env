e86ae5b5cda0f506456a0a1cd9329944f8fc9c1e
screfy <screfy@pm.me>
2022-03-06 15:48:26 +0100

    test: add tests for `json` validator

---
+++ test/validators/json.test.ts
@@ -0,0 +1,31 @@
+import { json, parse } from '../../src';
+import {
+  expectToStrictEqual,
+  expectToThrowErrorAndCallConsole
+} from '../helpers';
+
+test('validate json', () => {
+  const env = parse(
+    {
+      FOO: json<{ bar: string }>()
+    },
+    {
+      env: { FOO: JSON.stringify({ bar: 'baz' }) }
+    }
+  );
+
+  expectToStrictEqual<typeof env>(env, { FOO: { bar: 'baz' } });
+});
+
+test('fail with invalid json', () => {
+  expectToThrowErrorAndCallConsole(() =>
+    parse(
+      {
+        FOO: json<{ bar: string }>()
+      },
+      {
+        env: { FOO: 'bar' }
+      }
+    )
+  );
+});

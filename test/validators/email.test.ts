b460e1b51b9403e027b6f939530e2b01a8cec4b5
screfy <screfy@pm.me>
2022-03-06 15:26:25 +0100

    test: add tests for `email` validator

---
+++ test/validators/email.test.ts
@@ -0,0 +1,33 @@
+import { email, parse } from '../../src';
+import {
+  expectToStrictEqual,
+  expectToThrowErrorAndCallConsole
+} from '../helpers';
+
+const EXAMPLE_EMAIL = 'admin@domain.com';
+
+test('validate email', () => {
+  const env = parse(
+    {
+      FOO: email()
+    },
+    {
+      env: { FOO: EXAMPLE_EMAIL }
+    }
+  );
+
+  expectToStrictEqual<typeof env>(env, { FOO: EXAMPLE_EMAIL });
+});
+
+test('fail with invalid email', () => {
+  expectToThrowErrorAndCallConsole(() =>
+    parse(
+      {
+        FOO: email()
+      },
+      {
+        env: { FOO: 'bar' }
+      }
+    )
+  );
+});

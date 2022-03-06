8459ef88aedcf816a87a738e651cdb0d91488306
screfy <screfy@pm.me>
2022-03-06 15:37:11 +0100

    test: add tests for `literal` validator

---
+++ test/validators/literal.test.ts
@@ -0,0 +1,35 @@
+import { literal, parse } from '../../src';
+import {
+  expectToStrictEqual,
+  expectToThrowErrorAndCallConsole
+} from '../helpers';
+
+test('validate literal', () => {
+  const env = parse(
+    {
+      FOO: literal({
+        values: ['foo', 'bar']
+      })
+    },
+    {
+      env: { FOO: 'bar' }
+    }
+  );
+
+  expectToStrictEqual<typeof env>(env, { FOO: 'bar' });
+});
+
+test('fail with invalid value', () => {
+  expectToThrowErrorAndCallConsole(() =>
+    parse(
+      {
+        FOO: literal({
+          values: ['foo', 'bar']
+        })
+      },
+      {
+        env: { FOO: 'baz' }
+      }
+    )
+  );
+});

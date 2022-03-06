163672728aff2245c6bd2b62fd3cddc88cc5018d
screfy <screfy@pm.me>
2022-03-06 15:10:01 +0100

    test: add basic tests

---
+++ test/helpers.ts
@@ -0,0 +1,20 @@
+function mockConsole() {
+  const consoleMock = jest
+    .spyOn(console, 'error')
+    .mockImplementationOnce(() => {});
+
+  return consoleMock;
+}
+
+export function expectToThrowErrorAndCallConsole(fn: () => unknown) {
+  const consoleMock = mockConsole();
+
+  expect(() => fn()).toThrowError();
+  expect(consoleMock).toBeCalledTimes(1);
+
+  consoleMock.mockClear();
+}
+
+export function expectToStrictEqual<T>(actual: T, expected: T) {
+  expect(actual).toStrictEqual<T>(expected);
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,3 +1,43 @@
-test('two plus two is four', () => {
-  expect(2 + 2).toBe(4);
+import { num, parse, str } from '../src';
+import {
+  expectToStrictEqual,
+  expectToThrowErrorAndCallConsole
+} from './helpers';
+
+test('custom environment', () => {
+  const env = parse(
+    {
+      FOO: str()
+    },
+    {
+      env: { FOO: 'bar' }
+    }
+  );
+
+  expectToStrictEqual<typeof env>(env, { FOO: 'bar' });
+});
+
+test('missing variable', () => {
+  expectToThrowErrorAndCallConsole(() =>
+    parse(
+      {
+        FOO: str()
+      },
+      {
+        env: {}
+      }
+    )
+  );
+});
+
+test('default values', () => {
+  const env = parse({
+    FOO: str({ default: 'bar' }),
+    BAR: num({ default: 1 })
+  });
+
+  expectToStrictEqual<typeof env>(env, {
+    FOO: 'bar',
+    BAR: 1
+  });
 });

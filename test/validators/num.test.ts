124cc7a1e54d1fb666976fdadffa83b963fd4452
screfy <screfy@pm.me>
2022-03-06 15:14:47 +0100

    test: add tests for `num` validator

---
+++ test/validators/num.test.ts
@@ -0,0 +1,44 @@
+import { num, parse } from '../../src';
+import {
+  expectToStrictEqual,
+  expectToThrowErrorAndCallConsole
+} from '../helpers';
+
+test('validate number', () => {
+  const env = parse(
+    {
+      FOO: num()
+    },
+    {
+      env: { FOO: '1' }
+    }
+  );
+
+  expectToStrictEqual<typeof env>(env, { FOO: 1 });
+});
+
+test('fail with invalid value', () => {
+  expectToThrowErrorAndCallConsole(() =>
+    parse(
+      {
+        FOO: num()
+      },
+      {
+        env: { FOO: 'bar' }
+      }
+    )
+  );
+});
+
+test('fail with out of range', () => {
+  expectToThrowErrorAndCallConsole(() =>
+    parse(
+      {
+        FOO: num({ range: [1, 3] })
+      },
+      {
+        env: { FOO: '5' }
+      }
+    )
+  );
+});

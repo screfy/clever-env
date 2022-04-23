79e42acfa68a7dc4fdf331217ebb57684bbd619b
screfy <screfy@pm.me>
2022-04-23 16:44:11 +0200

    test: add tests for custom validator

---
+++ test/validators/custom-validator.test.ts
@@ -0,0 +1,68 @@
+import {
+	createValidator,
+	InvalidVariableError,
+	parse,
+	VariableOptions
+} from '../../src';
+import {
+	expectToStrictEqual,
+	expectToThrowErrorAndCallConsole
+} from '../__helpers__';
+
+interface LengthValidatorOptions extends VariableOptions<string> {
+	length?: number;
+}
+
+const lengthValidator = createValidator<string, LengthValidatorOptions>(
+	(key, value, { length }) => {
+		if (length && value.length !== length) {
+			throw new InvalidVariableError(key, `must be ${length} characters long`);
+		}
+
+		return value;
+	}
+);
+
+describe('Custom validator', () => {
+	test('validate provided value', () => {
+		const env = parse(
+			{
+				FOO: lengthValidator({ length: 6 })
+			},
+			{
+				env: { FOO: 'foobar' }
+			}
+		);
+
+		expectToStrictEqual<typeof env>(env, { FOO: 'foobar' });
+	});
+
+	test('fail with invalid value', () => {
+		expectToThrowErrorAndCallConsole(() => {
+			parse(
+				{
+					FOO: lengthValidator({ length: 6 })
+				},
+				{
+					env: { FOO: 'bar' }
+				}
+			);
+		});
+	});
+
+	test('validate default value', () => {
+		const env = parse({
+			FOO: lengthValidator({ length: 6, default: 'foobar' })
+		});
+
+		expectToStrictEqual<typeof env>(env, { FOO: 'foobar' });
+	});
+
+	test('fail with invalid default value', () => {
+		expectToThrowErrorAndCallConsole(() => {
+			parse({
+				FOO: lengthValidator({ length: 6, default: 'bar' })
+			});
+		});
+	});
+});
